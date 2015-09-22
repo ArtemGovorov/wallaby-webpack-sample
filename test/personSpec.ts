dd960923655a294b17d4587beee8afd98006ae9c
slikts <dabas@untu.ms>
2015-09-22 16:34:35 +0300

    * Changed the test and src .js files to .ts * Added configuration to wallaby webpack postprocessor to resolve and load .ts files * Added a rudimentary tsconfig.json

---
+++ test/personSpec.ts
@@ -0,0 +1,12 @@
+import Person from '../src/Person';
+
+console.log(Person);
+
+// var chai = require('chai');
+// var expect = chai.expect;
+//
+// describe('Person', () => {
+//   it('should report name', () => {
+//     expect(new Person('John').name).to.equal('John');
+//   });
+// }
--- wallaby.js
+++ wallaby.js
@@ -1,17 +1,29 @@
 var babel = require('babel');
 var wallabyWebpack = require('wallaby-webpack');
 
-var webpackPostprocessor = wallabyWebpack({});
+var webpackPostprocessor = wallabyWebpack({
+  resolve: {
+    extensions: ['', '.ts', '.js']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }
+    ]
+  }
+});
 
 module.exports = function () {
 
   return {
     files: [
-      { pattern: 'src/*.js', load: false }
+      { pattern: 'src/*.ts', load: false }
     ],
 
     tests: [
-      { pattern: 'test/*Spec.js', load: false }
+      { pattern: 'test/*Spec.ts', load: false }
     ],
 
     preprocessors: {
